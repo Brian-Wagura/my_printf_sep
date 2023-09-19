d13e5635c4a92c62891a133938f2a4cd7839a111
ko-nee <konemas10@gmail.com>
2023-09-19 16:16:55 -0700

    Update

--- funcs2.c
+++ funcs2.c
@@ -4,7 +4,10 @@
 * print_unprintable - Prints ascii codes in hexa of non printable chars
 * @types: Lista of arguments
 * @buffer: Buffer array to handle print
-*
+* @flags:  Calculates active flags
+* @width: get width
+* @precision: Precision specification
+* @size: Size specifier
 * Return: Number of chars printed
 */
 
@@ -41,7 +44,10 @@
 * print_pointer - Prints the value of a pointer variable
 * @types: List a of arguments
 * @buffer: Buffer array to handle print
-*
+* @flags:  Calculates active flags
+* @width: get width
+* @precision: Precision specification
+* @size: Size specifier
 * Return: Number of chars printed.
 */
 
@@ -89,6 +95,10 @@ width, flags, padding, extra_c, padding_start));
 * print_reverse - Prints reverse string.
 * @types: Lista of arguments
 * @buffer: Buffer array to handle print
+* @flags:  Calculates active flags
+* @width: get width
+* @precision: Precision specification
+* @size: Size specifier
 *
 * Return: Numbers of chars printed
 */
@@ -129,47 +139,48 @@ return (count);
 * print_rot13string - Print a string in rot13.
 * @types: Lista of arguments
 * @buffer: Buffer array to handle print
+* @size: Size specifier 
 *
 * Return: Numbers of chars printed
 */
 
-int print_rot13string(va_list types, char buffer[])
+int print_rot13string(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
-	 int flags, int width, int precision, int size;
-char x;
-char *str;
-unsigned int i, j;
-int count = 0;
-char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char x;
+	char *str;
+	unsigned int i, j;
+	int count = 0;
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-str = va_arg(types, char *);
-UNUSED(buffer);
-UNUSED(flags);
-UNUSED(width);
-UNUSED(precision);
-UNUSED(size);
+	str = va_arg(types, char *);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
 
-if (str == NULL)
-str = "(AHYY)";
-for (i = 0; str[i]; i++)
-{
-for (j = 0; in[j]; j++)
-{
-if (in[j] == str[i])
-{
-x = out[j];
-write(1, &x, 1);
-count++;
-break;
-}
-}
-if (!in[j])
-{
-x = str[i];
-write(1, &x, 1);
-count++;
-}
-}
-return (count);
+		if (str == NULL)
+		str = "(AHYY)";
+		for (i = 0; str[i]; i++)
+		{
+		for (j = 0; in[j]; j++)
+			{
+			if (in[j] == str[i])
+				{
+			x = out[j];
+			write(1, &x, 1);
+			count++;
+			break;
+				}
+			}
+			if (!in[j])
+		{
+		x = str[i];
+		write(1, &x, 1);
+		count++;
+		}
+	}
+	return (count);
 }
